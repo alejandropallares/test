981e8786798f9d66dce5c8d50e4097b9ac927042
alejandropallares <alejandropallares10@gmail.com>
2024-10-06 20:09:39 -0600

    Initial Commit

---
+++ test/ContentView.swift
@@ -0,0 +1,29 @@
+//
+//  ContentView.swift
+//  test
+//
+//  Created by Alejandro Pallares on 05/10/24.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        ZStack {
+            Color.blue
+                .ignoresSafeArea()
+                .opacity(0.5)
+            VStack {
+                Image(systemName: "globe")
+                    .imageScale(.large)
+                    .foregroundStyle(.tint)
+                Text("Hello, world!")
+            }
+            .padding()
+        }
+    }
+}
+
+#Preview {
+    ContentView()
+}
